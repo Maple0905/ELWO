9607ee0d85ce7e63bd25af6cfc90f73b05513976
Maple0905 <foreverNo1Techi@gmail.com>
2023-05-04 11:20:47 -0400

    working on demo

--- components/Product.tsx
+++ components/Product.tsx
@@ -14,9 +14,9 @@ interface IProductProps {
 
 const Product = (props: IProductProps) => {
   return (
-    <div className="col-span-1 rounded-lg border border-gray-300 bg-gray-100">
-      <Link href={`product/${props.product.id}`} className="px-5 py-5">
-        <div className="mb-5 mx-auto">
+    <div className="col-span-1 border border-gray-300 bg-gray-100">
+      <Link href={`product/${props.product.id}`} className="py-5">
+        <div className="py-5 mx-auto">
           <Image
             className="mx-auto px-5 md:px-0"
             src={props.product.url}
@@ -25,9 +25,16 @@ const Product = (props: IProductProps) => {
             height={150}
           />
         </div>
-        <div className="">
-          <p className="md:ml-3 px-5 m-0 max-w-[30ch] text-sm md:text-md lg:text-lg">{props.product.name}</p>
-          <p className="md:ml-3 px-5 m-0 max-w-[30ch] text-sm md:text-md lg:text-lg">{props.product.type}</p>
+        <div className="text-center">
+          <p className="text-sm font-semibold md:text-md lg:text-lg">{props.product.name}</p>
+          <p className="text-sm md:text-md lg:text-lg">{props.product.type}</p>
+        </div>
+        <div className="mt-3">
+          <div className="grid grid-cols-2">
+            <div className="col-start-2 col-span-1 text-center text-white py-2 bg-green-600">
+              Buy
+            </div>
+          </div>
         </div>
       </Link>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,48 @@
-import { IProduct } from '@/components/Product'
-import { GetStaticProps } from 'next'
-import ProductList from '@/components/ProductList'
 import Layout from '@/components/Layout'
 import { useState } from 'react'
+import { GetStaticProps } from 'next';
+import { IProduct } from '@/components/Product';
+import ProductList from '@/components/ProductList';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Home() {
 
-  const [ productName, setProductName ] = useState("");
+  const [searchProductName, setSearchProductName] = useState("");
+  const [searchProducts, setProducts] = useState<IProduct[]>([]);
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const filteredProducts = products.filter((product) => product.name.toLowerCase().includes(searchProductName.toLowerCase()));
+    setProducts(filteredProducts);
+  }
 
   return (
     <Layout>
-      <div className="max-w-7xl w-full">
-        <div className="mx-auto max-w-3xl w-full mb-5">
-          <div className="flex">
-            <input type="search" id="search-dropdown" className="block px-5 py-4 w-full text-lg text-gray-900 rounded-l-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Find your power tool by model, article number or EAN..." required onChange={(e) => setProductName(e.target.value)}/>
-            <button type="submit" className="flex px-5 py-4 text-lg text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
-              <svg aria-hidden="true" className="w-5 h-5 my-auto" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-              <span className="sr-only">&nbsp;Search</span>
-            </button>
-          </div>
-        </div>
-        {
-          productName == 'Bosch GSR 12V' ? 
-            (
-              <>
-                <div className="px-1 sm:px-5 py-5"><span className="text-lg">Please select your tool : </span></div>
-                <ProductList products={products} />
-              </>
-            ) : ''
-        }
+      <div className="max-w-5xl w-full">
+          {
+            searchProducts.length == 0 ? (
+              <form className="flex" onSubmit={handleSubmit}>
+                <input type="search" id="search-dropdown" className="block px-5 py-4 w-full text-lg text-gray-900 rounded-l-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Find your power tool by model, article number or EAN..." required value={searchProductName} onChange={(e) => setSearchProductName(e.target.value)} />
+                <button type="submit" className="flex px-5 py-4 text-lg text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                  <svg aria-hidden="true" className="w-5 h-5 my-auto" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                  <span className="sr-only">&nbsp;Search</span>
+                </button>
+              </form>
+            ) : (
+              <div>
+              <Link href={`/`}>
+                <div className="flex items-center mb-5">
+                  <Image src="/prev.png" alt="Prev Icon" width={20} height={20} />
+                  <p className="ml-2">Back</p>
+                </div>
+              </Link>
+                <input className="block px-5 py-4 w-full text-lg text-gray-900 rounded-l-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" value={searchProductName} disabled />
+                <p className="py-5 px-5">Please select your tool</p>
+                <ProductList products={searchProducts} />
+              </div>
+            )
+          }
       </div>
     </Layout>
   )
@@ -38,49 +52,49 @@ export const products: IProduct[] = [
   {
     id: "1",
     name: "Bosch GSR 12V",
-    type: "-15 FC",
+    type: "15CV 2426",
     url: "/product.png"
   },
   {
     id: "2",
     name: "Bosch GSR 12V",
-    type: "-15",
+    type: "15CV 2426",
     url: "/product.png"
   },
   {
     id: "3",
     name: "Bosch GSR 12V",
-    type: "-35",
+    type: "15CV 2426",
     url: "/product.png"
   },
   {
     id: "4",
     name: "Bosch GSR 12V",
-    type: "-35 FC",
+    type: "15CV 2426",
     url: "/product.png"
   },
   {
     id: "5",
     name: "Bosch GSR 12V",
-    type: "-35 HX",
+    type: "15CV 2426",
     url: "/product.png"
   },
   {
     id: "6",
     name: "Bosch GSR 12V",
-    type: "-15 FC",
+    type: "15CV 2426",
     url: "/product.png"
   },
   {
     id: "7",
     name: "Bosch GSR 12V",
-    type: "-15 FC",
+    type: "15CV 2426",
     url: "/product.png"
   },
   {
     id: "8",
     name: "Bosch GSR 12V",
-    type: "-15 FC",
+    type: "15CV 2426",
     url: "/product.png"
   },
 ]
